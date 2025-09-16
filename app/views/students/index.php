d89125c7872015f5657d16aaa0582916437192e2
larosa_ryza_anne <larosaryxaanne@gmail.com>
2025-09-16 15:00:18 +0800

    Foorsy

--- app/views/students/index.php
+++ app/views/students/index.php
@@ -11,12 +11,12 @@
         <h1 class="text-3xl font-bold mb-6 text-pink-500">Students List</h1>
 
         <div class="flex gap-3">
-            <a href="/students/create" 
+            <a href="<?= site_url('students/create') ?>" 
                class="bg-blue-300 hover:bg-blue-400 text-white px-5 py-2 rounded-lg shadow">
                 + Add Student
             </a>
 
-            <a href="/students/delete_all"
+            <a href="<?= site_url('students/delete_all') ?>"
                onclick="return confirm('Are you sure you want to delete ALL records?')"
                class="bg-pink-300 hover:bg-pink-400 text-white px-5 py-2 rounded-lg shadow">
                 Delete All
@@ -39,16 +39,16 @@
                     <?php if (!empty($students)): ?>
                         <?php foreach ($students as $student): ?>
                             <tr class="hover:bg-pink-50/50">
-                                <td class="py-2 px-4 border-b border-blue-200"><?= $student['id'] ?></td>
-                                <td class="py-2 px-4 border-b border-blue-200"><?= $student['last_name'] ?></td>
-                                <td class="py-2 px-4 border-b border-blue-200"><?= $student['first_name'] ?></td>
-                                <td class="py-2 px-4 border-b border-blue-200"><?= $student['email'] ?></td>
+                                <td class="py-2 px-4 border-b border-blue-200"><?= html_escape($student['id']) ?></td>
+                                <td class="py-2 px-4 border-b border-blue-200"><?= html_escape($student['last_name']) ?></td>
+                                <td class="py-2 px-4 border-b border-blue-200"><?= html_escape($student['first_name']) ?></td>
+                                <td class="py-2 px-4 border-b border-blue-200"><?= html_escape($student['email']) ?></td>
                                 <td class="py-2 px-4 border-b border-blue-200 text-center">
-                                    <a href="/students/edit/<?= $student['id'] ?>" 
+                                    <a href="<?= site_url('students/edit/'.$student['id']) ?>" 
                                        class="bg-blue-300 hover:bg-blue-400 text-white px-3 py-1 rounded-lg shadow">
                                         Edit
                                     </a>
-                                    <a href="/students/delete/<?= $student['id'] ?>" 
+                                    <a href="<?= site_url('students/delete/'.$student['id']) ?>" 
                                        onclick="return confirm('Are you sure you want to delete this student?')"
                                        class="bg-pink-300 hover:bg-pink-400 text-white px-3 py-1 rounded-lg shadow ml-1">
                                         Delete
